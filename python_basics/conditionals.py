e3ca337dcc55f7f9482698336fb86c4917bd2c3b
Tufan Düzel <tufansahind@gmail.com>
2025-01-22 17:34:40 +0100

    python_basics

---
+++ python_basics/conditionals.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 20
+
+if number1 > number2:
+    print("Number 1 is bigger than number 2.")
+elif number1 == number2:
+    print("These two numbers are equal to each other.")
+else:
+    print("Number 2 is bigger than number 1.")
---
+++ python_basics/dictionary.py
@@ -0,0 +1,12 @@
+dictionary = {
+       "table" : "masa",
+       "book" : "kitap"
+}
+
+print(dictionary)
+print(dictionary["table"])
+
+dictionary["pencil"] = "kalem"
+del(dictionary["table"])
+
+print(dictionary)
