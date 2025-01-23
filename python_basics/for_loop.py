c13895ce2c405636cd4cbd440458bb1aac9c61e6
Tufan Düzel <tufansahind@gmail.com>
2025-01-23 09:19:09 +0100

    python_basics

---
+++ python_basics/for_loop.py
@@ -0,0 +1,15 @@
+cities = ["New York", "Barcelona", "Paris"]
+
+#%%
+for city in cities:
+    print(city)
+ 
+#%%    
+for x in range(100):
+    print(x)
+    
+for x in range(1,10):
+        print(x)
+        
+for x in range(1,10,2):
+        print(x)
---
+++ python_basics/while_loop.py
@@ -0,0 +1,8 @@
+count = 1
+result = 0
+
+while count <= 10:
+    result = result + count
+    count = count + 1
+    
+print(result)
